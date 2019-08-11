6218b522c3854d54122c4e38908db41ab194536d
Christian Martins <christian@labdynamo.com>
2019-08-10 23:39:44 -0400

    commiting graphql prisma code

--- main.js
+++ main.js
@@ -1,64 +1,28 @@
 require("dotenv").config();
-import express from "express";
-import path from "path";
+import { prisma } from "./src/generated/prisma-client";
 import { GraphQLServer } from "graphql-yoga";
-import favicon from "serve-favicon";
-import logger from "morgan";
-import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
-import cors from "cors";
-import fileUpload from "express-fileupload";
 
-let links = [
-  {
-    id: "link-1",
-    url: "www.sparkv2.website",
-    description: "Something to talk about"
-  }
-];
-let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API for a simple`,
-    feed: () => links
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    }
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      });
     }
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: { prisma }
 });
 
 server.start(() => console.log(`Server is running on localhost:80`));
-// let reactBuildDirectory = "../droplet-client/build";
-// const app = express();
-
-// app.set("port", process.env.PORT || 80);
-// app.use(favicon(reactBuildDirectory + "/favicon.ico"));
-// app.use(logger("dev"));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cookieParser());
-// app.use(cors());
-// app.use(fileUpload());
-// app.use(express.static(reactBuildDirectory));
-
-// app.get("/", function(request, response) {
-//   response.sendFile("index.html", { root: reactBuildDirectory });
-//   response.sendFile("Hello World");
-// });
-
-// app.listen(app.get("port"), function() {
-//   console.log("Node app is running at localhost:" + app.get("port"));
-// });
